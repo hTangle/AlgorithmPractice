5f5efc667a286366c7b49a769246282658979bac
HZW <hsup1994@hotmail.com>
2019-06-25 21:50:04 +0800

    update

---
+++ src/com/nevergetme/algorithmCompetition/First/Offer.java
@@ -0,0 +1,118 @@
+package com.nevergetme.algorithmCompetition.First;
+
+import java.util.ArrayList;
+
+
+/**
+ * Create by Alden He on 2019/6/25
+ */
+public class Offer {
+    public static void main(String[] args){
+        TreeNode root=new Offer().reConstructBinaryTree(new int[]{1,2,4,7,3,5,6,8},new int[]{4,7,2,1,5,3,8,6});
+        System.out.println();
+    }
+//    public int minNumberInRotateArray(int [] array) {
+//        int begin=0,end=array.length-1;
+//        while (begin<=end){
+//            int mid=(begin+end)>>1;
+//            if(array[begin]<array[mid]){
+//                begin=mid+1;
+//            }else if()
+//        }
+//    }
+    public TreeLinkNode GetNext(TreeLinkNode pNode)
+    {
+        if(pNode==null)return null;
+        if(pNode.right!=null){
+            pNode=pNode.right;
+            while (pNode.left!=null)pNode=pNode.left;
+            return pNode;
+        }else{
+            while (pNode.next!=null){
+                //如果为父节点的左节点，则下一个节点为父节点
+                if(pNode.next.left==pNode){
+                    return pNode.next;
+                }
+                pNode=pNode.next;
+            }
+            return pNode.next;
+        }
+    }
+    public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+        //首先取pre的第一个节点，然后找到第一个节点在in中的位置，然后切分即可
+        return reConstructBinaryTree(pre,in,0,pre.length-1,0,in.length-1);
+    }
+    public TreeNode reConstructBinaryTree(int [] pre,int [] in,int preBegin,int preEnd,int inBegin,int inEnd) {
+        TreeNode root=new TreeNode(pre[preBegin]);
+        int rootIndex=-1;
+        for(int i=inBegin;i<=inEnd;i++){
+            if(in[i]==pre[preBegin]){
+                rootIndex=i;
+                break;
+            }
+        }
+        if(rootIndex>inBegin){
+            root.left=reConstructBinaryTree(pre,in,preBegin+1,rootIndex-inBegin+preBegin,inBegin,rootIndex-1);
+        }
+        if(rootIndex<inEnd){
+            root.right=reConstructBinaryTree(pre,in,rootIndex-inBegin+preBegin+1,preEnd,rootIndex+1,inEnd);
+        }
+        return root;
+    }
+    public boolean duplicate(int numbers[], int length, int[] duplication) {
+        int begin = 1, end = length - 1;
+        while (end >= begin) {
+            int mid = (begin + end) >> 1;
+            int count = 0;
+            for (int k : numbers) {
+                if (k <= mid && k >= begin) {
+                    count++;
+                }
+            }
+            if (end == begin) {
+                if (count > 1) {
+                    duplication[0] = begin;
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            if (count > (mid - begin + 1)) end = mid;
+            else begin = mid + 1;
+        }
+        return false;
+//        for(int i=0;i<length;i++){
+//            if(numbers[i]!=i){
+//                if(numbers[numbers[i]]==numbers[i]){
+//                    duplication[0]=numbers[i];
+//                    return true;
+//                }else{
+//                    int temp=numbers[numbers[i]];
+//                    numbers[numbers[i]]=numbers[i];
+//                    numbers[i]=temp;
+//                }
+//            }
+//        }
+//        return false;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    public class TreeLinkNode {
+        int val;
+        TreeLinkNode left = null;
+        TreeLinkNode right = null;
+        TreeLinkNode next = null;
+
+        TreeLinkNode(int val) {
+            this.val = val;
+        }
+    }
+}
--- src/com/nevergetme/algorithmCompetition/Solution.java
+++ src/com/nevergetme/algorithmCompetition/Solution.java
@@ -65,9 +65,38 @@ public class Solution {
 //        System.out.println(solution.findMin(new int[]{3,5,1}));
         System.out.println();
         solution.textTreeMap();
+//        Thread
 //        System.out.println(matcher.toString());
 //        ArrayList
     }//77260018937180
+
+    public int superEggDrop(int K, int N) {
+        // Right now, dp[i] represents dp(1, i)
+        int[] dp = new int[N+1];
+        for (int i = 0; i <= N; ++i)
+            dp[i] = i;
+
+        for (int k = 2; k <= K; ++k) {
+            // Now, we will develop dp2[i] = dp(k, i)
+            int[] dp2 = new int[N+1];
+            int x = 1;
+            for (int n = 1; n <= N; ++n) {
+                // Let's find dp2[n] = dp(k, n)
+                // Increase our optimal x while we can make our answer better.
+                // Notice max(dp[x-1], dp2[n-x]) > max(dp[x], dp2[n-x-1])
+                // is simply max(T1(x-1), T2(x-1)) > max(T1(x), T2(x)).
+                while (x < n && Math.max(dp[x-1], dp2[n-x]) > Math.max(dp[x], dp2[n-x-1]))
+                    x++;
+
+                // The final answer happens at this x.
+                dp2[n] = 1 + Math.max(dp[x-1], dp2[n-x]);
+            }
+
+            dp = dp2;
+        }
+
+        return dp[N];
+    }
 
     public boolean canIWin(int maxChoosableInteger, int desiredTotal) {
         if(desiredTotal<=maxChoosableInteger)return true;
@@ -113,9 +142,9 @@ public class Solution {
         map.put(2,"2");
         map.put(1,"1");
         map.put(3,"3");
-        map.forEach((Integer i,String s)->{
-            System.out.println(i+"---"+s);
-        });
+        map.forEach((Integer i,String s)->
+            System.out.println(i+"---"+s)
+        );
     }
 
     public double new21Game(int N, int K, int W) {

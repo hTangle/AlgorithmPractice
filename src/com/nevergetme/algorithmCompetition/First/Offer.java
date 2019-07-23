50777d3e499e028a4498ff23b7f639c897366d49
Alden <hsup1994@hotmail.com>
2019-07-23 22:30:33 +0800

    update

--- src/com/nevergetme/algorithmCompetition/First/Offer.java
+++ src/com/nevergetme/algorithmCompetition/First/Offer.java
@@ -7,11 +7,12 @@ import java.util.ArrayList;
  * Create by Alden He on 2019/6/25
  */
 public class Offer {
-    public static void main(String[] args){
-        TreeNode root=new Offer().reConstructBinaryTree(new int[]{1,2,4,7,3,5,6,8},new int[]{4,7,2,1,5,3,8,6});
-        System.out.println();
+    public static void main(String[] args) {
+//        TreeNode root = new Offer().reConstructBinaryTree(new int[]{1, 2, 4, 7, 3, 5, 6, 8}, new int[]{4, 7, 2, 1, 5, 3, 8, 6});
+        System.out.println(new Offer().canPartition(new int[]{1, 5, 11, 5}));
     }
-//    public int minNumberInRotateArray(int [] array) {
+
+    //    public int minNumberInRotateArray(int [] array) {
 //        int begin=0,end=array.length-1;
 //        while (begin<=end){
 //            int mid=(begin+end)>>1;
@@ -20,45 +21,82 @@ public class Offer {
 //            }else if()
 //        }
 //    }
-    public TreeLinkNode GetNext(TreeLinkNode pNode)
-    {
-        if(pNode==null)return null;
-        if(pNode.right!=null){
-            pNode=pNode.right;
-            while (pNode.left!=null)pNode=pNode.left;
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = 0;
+        for (int z : nums) sum += z;
+        if (sum % k != 0) return false;
+        boolean[] isVisited=new boolean[nums.length];
+        return canPartitionKSubsets(nums,k,sum/k,0,0,isVisited);
+    }
+    public boolean canPartitionKSubsets(int[] nums,int k,int target,int start,int curSum,boolean[] isVisited){
+        if(k==1)return true;
+        if(curSum==target)return canPartitionKSubsets(nums,k-1,target,0,0,isVisited);
+        if(curSum>target)return false;
+        for(int i=start;i<nums.length;i++){
+            if(isVisited[i])continue;
+            isVisited[i]=true;
+            if(canPartitionKSubsets(nums,k,target,i+1,curSum+nums[i],isVisited))return true;
+            isVisited[i]=false;
+        }
+        return false;
+    }
+
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int k : nums) sum += k;
+        if (sum % 2 == 1) return false;
+        sum = sum >> 1;
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+        for (int num : nums) {
+            for (int i = sum; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+        return dp[sum];
+    }
+
+    public TreeLinkNode GetNext(TreeLinkNode pNode) {
+        if (pNode == null) return null;
+        if (pNode.right != null) {
+            pNode = pNode.right;
+            while (pNode.left != null) pNode = pNode.left;
             return pNode;
-        }else{
-            while (pNode.next!=null){
+        } else {
+            while (pNode.next != null) {
                 //如果为父节点的左节点，则下一个节点为父节点
-                if(pNode.next.left==pNode){
+                if (pNode.next.left == pNode) {
                     return pNode.next;
                 }
-                pNode=pNode.next;
+                pNode = pNode.next;
             }
             return pNode.next;
         }
     }
-    public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
         //首先取pre的第一个节点，然后找到第一个节点在in中的位置，然后切分即可
-        return reConstructBinaryTree(pre,in,0,pre.length-1,0,in.length-1);
+        return reConstructBinaryTree(pre, in, 0, pre.length - 1, 0, in.length - 1);
     }
-    public TreeNode reConstructBinaryTree(int [] pre,int [] in,int preBegin,int preEnd,int inBegin,int inEnd) {
-        TreeNode root=new TreeNode(pre[preBegin]);
-        int rootIndex=-1;
-        for(int i=inBegin;i<=inEnd;i++){
-            if(in[i]==pre[preBegin]){
-                rootIndex=i;
+
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in, int preBegin, int preEnd, int inBegin, int inEnd) {
+        TreeNode root = new TreeNode(pre[preBegin]);
+        int rootIndex = -1;
+        for (int i = inBegin; i <= inEnd; i++) {
+            if (in[i] == pre[preBegin]) {
+                rootIndex = i;
                 break;
             }
         }
-        if(rootIndex>inBegin){
-            root.left=reConstructBinaryTree(pre,in,preBegin+1,rootIndex-inBegin+preBegin,inBegin,rootIndex-1);
+        if (rootIndex > inBegin) {
+            root.left = reConstructBinaryTree(pre, in, preBegin + 1, rootIndex - inBegin + preBegin, inBegin, rootIndex - 1);
         }
-        if(rootIndex<inEnd){
-            root.right=reConstructBinaryTree(pre,in,rootIndex-inBegin+preBegin+1,preEnd,rootIndex+1,inEnd);
+        if (rootIndex < inEnd) {
+            root.right = reConstructBinaryTree(pre, in, rootIndex - inBegin + preBegin + 1, preEnd, rootIndex + 1, inEnd);
         }
         return root;
     }
+
     public boolean duplicate(int numbers[], int length, int[] duplication) {
         int begin = 1, end = length - 1;
         while (end >= begin) {
@@ -105,6 +143,7 @@ public class Offer {
             val = x;
         }
     }
+
     public class TreeLinkNode {
         int val;
         TreeLinkNode left = null;

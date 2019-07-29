8fd4ce6ad4e226aed910bb870c270b91005a0bca
Alden <hsup1994@hotmail.com>
2019-07-29 22:23:58 +0800

    update

--- src/com/nevergetme/algorithmCompetition/Solution.java
+++ src/com/nevergetme/algorithmCompetition/Solution.java
@@ -70,6 +70,14 @@ public class Solution {
 //        ArrayList
     }//77260018937180
 
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null||p==root||q==root)return root;
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        if(left!=null&&right!=null)return root;
+        else return left==null?right:left;
+    }
+
     class Money{
         int notUse=0;
         int use=0;
